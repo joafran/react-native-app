27ab341d84a5195e2d0bfd77092ebf9a45cd8c15
joafran <joafran0016@gmail.com>
2021-05-27 11:15:25 -0300

    react-native app init

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import act1 from '@assets/a.jpg'
+const App = () => {
+  return (
+    <View>
+      <Text>HELLO WORLD FROM REACT NATIVE</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default App;
